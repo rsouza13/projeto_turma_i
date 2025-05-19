cbbcc31fc9b2334f658a81b5f9836aa64c2ff6e0
Tiago Rodrigues <tiagorsouza13@gmail.com>
2025-05-19 19:59:26 -0300

    Features:   1 - Inclusão de mascara no formulário de cadastro   2 - DataTable na tabela de alunos;

--- index.php
+++ index.php
@@ -14,22 +14,28 @@ $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Alunos</title>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.1/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.3.1/js/dataTables.js"></script>
     <link rel="stylesheet" href="style.css" />
     <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
     <h1 id="tituloh1">Lista de Alunos</h1>
     <p><a href="formCadastrarAluno.html">Novo Aluno</a></p>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Foto</th>
-            <th>Nome</th>
-            <th>Altura</th>
-            <th>Data de Nascimento</th>
-            <th>Sexo</th>
-            <th>Ação</th>
-        </tr>
+    <table id="tableAlunos" class="stripe">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Foto</th>
+                <th>Nome</th>
+                <th>Altura</th>
+                <th>Data de Nascimento</th>
+                <th>Sexo</th>
+                <th>Ação</th>
+            </tr>
+        </thead>
+        <tbody>
         <?php
             while($result = $stmt->fetch()){
                 echo '<tr>';
@@ -47,6 +53,16 @@ $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
                 echo '</tr>';
             }
         ?>
+        </tbody>
     </table>
+    <script>
+        $(document).ready( function () {
+            $('#tableAlunos').DataTable({
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/2.3.1/i18n/pt-BR.json',
+                }
+            });
+        } );
+    </script>
 </body>
 </html>
